7029d0e067586a2ab7d785b11bcf747e4ee92b3d
g30021214 <guokainan@huawei.com>
2024-03-08 09:11:37 +0800

    change eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -23,28 +23,6 @@ export default antfu({
       'ts/no-throw-literal': 'off',
     },
   },
-  vue: {
-    overrides: {
-      'vue/html-closing-bracket-newline': [
-        'error',
-        {
-          singleline: 'never',
-          multiline: 'never',
-        },
-      ],
-
-      // 单行标签最多允许 3 个属性
-      'vue/max-attributes-per-line': [
-        'error',
-        {
-          singleline: { max: 3 },
-          multiline: { max: 1 },
-        },
-      ],
-
-      'vue/operator-linebreak': ['error', 'after'],
-    },
-  },
   rules: {
     'style/operator-linebreak': ['error', 'after'],
   },
--- packages/imd-fetcher/src/utils/workspace.ts
+++
@@ -1,3 +0,0 @@
-export function initWorkspace(dir: string) {
-
-}
